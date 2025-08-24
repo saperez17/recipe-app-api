d65b3c04256010fea5705cee9d6f5d4f8fcc679e
Santiago <usuario@MacBook-Pro-de-MilenioPc.local>
2025-08-23 20:31:48 -0500

    Configured docker compose and checks wait for db

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,10 +2,12 @@
 Calculator functions
 """
 
-def add (x,y):
-    """ Add x and y and returns result """
-    return x + y;
 
-def subtract (x, y):
+def add(x, y):
+    """Add x and y and returns result """
+    return x + y
+
+
+def subtract(x, y):
     """Subtract x from y and returns result"""
     return y-x
--- app/app/tests.py
+++ app/app/tests.py
@@ -4,7 +4,9 @@ Sample tests
 
 from django.test import SimpleTestCase
 
+
 from app import calc
+
 
 class CalcTests(SimpleTestCase):
     """test the Calc module"""
@@ -12,12 +14,11 @@ class CalcTests(SimpleTestCase):
     def test_add_numbers(self):
         """Test adding numbers together"""
 
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
         self.assertEqual(res, 11)
-    
+
     def test_subtract_numbers(self):
         """Test subtracting numbers"""
 
         res = calc.subtract(10, 15)
         self.assertEqual(res, 5)
-        
